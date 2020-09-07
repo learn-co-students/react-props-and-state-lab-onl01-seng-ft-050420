987258c0f3e0266522178c627da0c50fc2dc238b
uharston <uharston@gmail.com>
2020-09-07 13:31:36 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+  handleChangeType = (e) => {
+    this.setState({
+      filters: {type: e.target.value }
+    })
+  }
+
+  handlePetsClick = () => {
+    let baseURL = "/api/pets"
+    if(this.state.filters.type !== 'all') {
+      baseURL = baseURL + `?type=${this.state.filters.type}` 
+    }
+    console.log(baseURL)
+    fetch(baseURL)
+      .then(resp => resp.json())
+      .then(res => this.updatePets(res))
+
+
+  }
+
+  updatePets(pet) {
+    this.setState({pets: pet })
+  }
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets: pets });
+  }
+
+
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChangeType} onFindPetsClick={this.handlePetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
